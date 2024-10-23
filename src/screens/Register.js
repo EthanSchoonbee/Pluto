22d0487ff7e19bb2709071a22ea5b26eacf946b2
Cameron Brooks <cbrooks02work@gmail.com>
2024-10-23 22:09:32 +0200

    Loading bars setupo for log/reg

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, KeyboardAvoidingView, ScrollView, Platform, ImageBackground, StatusBar } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform,
+    ImageBackground,
+    StatusBar,
+    ActivityIndicator
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import strings from "../strings/en";
 import firebaseService from '../services/firebaseService';
@@ -13,12 +25,14 @@ const RegisterScreen = ({ navigation }) => {
     const [location, setLocation] = useState('');
     const [password, setPassword] = useState('');
     const [secureTextEntry, setSecureTextEntry] = useState(true);
+    const [loading, setLoading] = useState(false);  // Add loading state
 
     const togglePasswordVisibility = () => {
         setSecureTextEntry(!secureTextEntry);
     };
 
     const handleRegister = async () => {
+
         if (ValidationClass.isEmptyOrWhitespace(fullName) ||
             ValidationClass.isEmptyOrWhitespace(email) ||
             ValidationClass.isEmptyOrWhitespace(password) ||
@@ -38,8 +52,11 @@ const RegisterScreen = ({ navigation }) => {
             return;
         }
 
+        setLoading(true);
         try {
+
             const user = await firebaseService.registerUser(fullName, email, password, phoneNo, location);
+            setLoading(false);
             console.log('User registered:', user);
             navigation.navigate('UserHome');
         } catch (error) {
@@ -131,6 +148,13 @@ const RegisterScreen = ({ navigation }) => {
                 <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
                     <Text style={styles.registerText}>{strings.register_button}</Text>
                 </TouchableOpacity>
+
+                {loading && (  // Display loading indicator when loading state is true
+                    <View style={styles.loadingContainer}>
+                        <ActivityIndicator size="large" color="#FFD700" />
+                        <Text>Logging in...</Text>
+                    </View>
+                )}
 
                 <Text style={styles.loginText}>
                     {strings.already_have_account}{'\n'}
