0389a7c38f8cba69970a6d41b76c83a934283036
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-10-23 22:30:30 +0200

    Fixed ShelterHomePage Image Download Directory Error Test 6 Pulling From Cloud

--- src/screens/ShelterHomePage.js
+++ src/screens/ShelterHomePage.js
@@ -1,24 +1,15 @@
-import React, {
-    useCallback,
-    useEffect,
-    useState
-} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
     View,
     Text,
-    Image,
     SafeAreaView,
     ScrollView,
     StatusBar,
-    ActivityIndicator
+    ActivityIndicator,
 } from 'react-native';
 import styles from '../styles/ShelterHomePageStyle';
 import Header from "../components/Header";
-import Navbar from "../components/ShelterNavbar";
-import {
-    useNavigation,
-    useFocusEffect
-} from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { Platform } from 'react-native';
 import { db, auth } from '../services/firebaseConfig';
 import {
@@ -28,15 +19,13 @@ import {
     where,
 } from 'firebase/firestore';
 import AnimalCard from '../components/AnimalCard';
-import { getLocalImageUrl, deleteLocalImage } from '../utils/imageUtils';
-import {  handleAdopt, handleDelete } from '../services/databaseService';
+import { handleAdopt, handleDelete } from '../services/databaseService';
 import NavbarWrapper from "../components/NavbarWrapper";
 
 const ShelterHomeScreen = () => {
     const navigation = useNavigation();
     const [animals, setAnimals] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [imagesLoaded, setImagesLoaded] = useState(false);
 
     const fetchAnimals = useCallback(async () => {
         setLoading(true);
@@ -55,9 +44,8 @@ const ShelterHomeScreen = () => {
 
             const unsubscribe = onSnapshot(animalsQuery, async (querySnapshot) => {
                 try {
-                    const animalsList = await Promise.all(querySnapshot.docs.map(async (doc) => {
+                    const animalsList = querySnapshot.docs.map((doc) => {
                         const data = doc.data();
-                        const localImageUrl = await getLocalImageUrl(data.imageUrls || []);
                         return {
                             id: doc.id,
                             name: data.name,
@@ -65,12 +53,11 @@ const ShelterHomeScreen = () => {
                             gender: data.gender,
                             breed: data.breed,
                             adoptionStatus: data.adoptionStatus,
-                            imageUrl: localImageUrl, // Store only the first image URL
+                            imageUrl: data.imageUrls[0] || '', // Store only the first image URL
                             notificationCount: data.notificationCount || 0,
                         };
-                    }));
+                    });
                     setAnimals(animalsList);
-                    await preloadImages(animalsList);
                 } catch (error) {
                     console.error('Error processing animals data:', error);
                 } finally {
@@ -88,17 +75,6 @@ const ShelterHomeScreen = () => {
         }
     }, []);
 
-
-    const preloadImages = async (animalsList) => {
-        const imageUrls = animalsList.flatMap(animal => animal.imageUrls).filter(url => url);
-        try {
-            await Promise.all(imageUrls.map(url => Image.prefetch(url)));
-            setImagesLoaded(true);
-        } catch (error) {
-            console.error('Error prefetching images:', error);
-        }
-    };
-
     const handleViewLikes = (animalId) => {
         navigation.navigate('ShelterChats', { animalId });
     };
@@ -109,17 +85,13 @@ const ShelterHomeScreen = () => {
         }, [fetchAnimals])
     );
 
-    useEffect(() => {
-        console.log('Loading state: ', loading);
-    }, [loading]); // Log loading state whenever it changes
-
     return (
-        <SafeAreaView style={[styles.container, {paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0}]} edges={['left', 'right']}>
+        <SafeAreaView style={[styles.container, { paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }]} edges={['left', 'right']}>
             <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
             <Header rightComponent={() => (
                 <Text style={styles.addButton} onPress={() => navigation.navigate('AddAnimal')}>Add</Text>
             )} />
-            {loading || !imagesLoaded ? (
+            {loading ? (
                 <View style={styles.loadingContainer}>
                     <ActivityIndicator size="large" color={'#d9cb94'} />
                     <Text>Loading...</Text>
