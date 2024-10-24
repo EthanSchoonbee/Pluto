bd1f6921b7b92fbf0c30386a0f8483801cc39b6f
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-10-24 21:19:00 +0200

    Fixed UserModel for Auth

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,25 +1,24 @@
 // Adjust UserModel constructor to handle nested preferences
 class UserModel {
-    constructor(uid, profileImage, email, fullName, phoneNo, selectedProvince, role, likedAnimals = [], preferences) {
+
+    constructor(uid, email, fullName, phoneNo, location, role, likedAnimals = [], animalType, breed, gender, province, ageRange, activityLevel, size, furColors = []) {
         this.uid = uid;
-        this.profileImage = profileImage;
         this.email = email;
         this.fullName = fullName;
         this.phoneNo = phoneNo;
-        this.selectedProvince = selectedProvince;
+        this.location = location;
         this.role = role;
         this.likedAnimals = likedAnimals;
-
-        // Assign preferences if passed as an object
-        if (preferences) {
-            this.animalType = preferences.animalType || 'Dog';
-            this.breed = preferences.breed || 'Any';
-            this.gender = preferences.gender || 'Any';
-            this.province = preferences.province;
-            this.ageRange = preferences.ageRange || [0, 20];
-            this.activityLevel = preferences.activityLevel || 0;
-            this.size = preferences.size || 1;
-            this.furColors = preferences.furColors || 'Any';
-        }
+        this.animalType = animalType;
+        this.breed = breed;
+        this.gender = gender;
+        this.province = province;
+        this.ageRange = ageRange;
+        this.activityLevel = activityLevel;
+        this.size = size;
+        this.furColors = furColors;
     }
+
 }
+
+export default UserModel;
