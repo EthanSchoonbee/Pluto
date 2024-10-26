178e168831efd4a3263bfcd165a93cf8350a9643
Caleb Baard <wordwarriors52@yq0fx.onmicrosoft.com>
2024-10-26 16:23:50 +0200

    a user can view the information for the user that is interested in adopting the animal

--- src/screens/UserDetailsForm.js
+++ src/screens/UserDetailsForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     View,
     Text,
@@ -9,6 +9,7 @@ import {
     TouchableOpacity,
     Modal,
     FlatList,
+    ActivityIndicator,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 //imports the safe area wrapper component
@@ -23,52 +24,142 @@ import {
     getRandomEmailSubject,
     getRandomEmailBody,
 } from "../utils/notificationMessages";
+import { useRoute } from "@react-navigation/native";
+import { getDoc, doc, onSnapshot } from "firebase/firestore";
+import { db, storage } from "../services/firebaseConfig";
+import * as FileSystem from "expo-file-system";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const UserDetailForm = ({ route, navigation }) => {
-    //extracts the pet name, pet image, user name , user image and the user id
-    const { petName, petImage, userName, userImage, userId } = route.params;
+    //passing the user id and animal id from the interested adopters page through navigation
+    const { userId, animalId } = route.params;
+    //states to hold the user details and animal details
+    const [userDetails, setUserDetails] = useState(null);
+    const [animalDetails, setAnimalDetails] = useState(null);
+    //states to handle the visibility of the notification modal and the selection modal
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isSelectionModalVisible, setIsSelectionModalVisible] = useState(false);
+    //state to hold the selected status
+    const [selectedStatus, setSelectedStatus] = useState(null);
+    //state to handle the loading of the data
+    const [loading, setLoading] = useState(true);
 
-    // Dummy user details object
-    //takes the user id and returns the user details
-    const userDetailsDatabase = {
-        1: {
-            email: "johnsmith@example.com",
-            phone: "+1 (555) 123-4567",
-            address: "123 Main St, Anytown, USA",
-            occupation: "Software Developer",
-            reasonForAdoption: "Looking for a loving companion",
-            previousPetExperience: "Owned 2 dogs in the past",
-        },
-        2: {
-            email: "janesmith@example.com",
-            phone: "+1 (555) 987-6543",
-            address: "456 Elm St, Another Town, USA",
-            occupation: "Teacher",
-            reasonForAdoption: "Want to give a pet a forever home",
-            previousPetExperience: "First-time pet owner",
-        },
-        3: {
-            email: "mikejohnson@example.com",
-            phone: "+1 (555) 246-8135",
-            address: "789 Oak St, Somewhere City, USA",
-            occupation: "Nurse",
-            reasonForAdoption: "Looking for a companion for my other dog",
-            previousPetExperience: "Currently own 1 dog",
-        },
-        4: {
-            email: "emilybrown@example.com",
-            phone: "+1 (555) 369-2580",
-            address: "101 Pine St, Elsewhere Village, USA",
-            occupation: "Graphic Designer",
-            reasonForAdoption: "Always wanted a pet since childhood",
-            previousPetExperience: "Grew up with family pets",
-        },
+    //fetching the user details and the animal details
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true); //setting the current loading state to true
+            const userUnsubscribe = onSnapshot(
+                doc(db, "users", userId),
+                async (userDoc) => {
+                    if (userDoc.exists()) {
+                        const userData = userDoc.data();
+                        const userImage = await fetchAndCacheImage(
+                            userData.profileImage,
+                            "user"
+                        );
+                        setUserDetails({ ...userData, image: userImage });
+                    } else {
+                        console.warn("User document does not exist");
+                        setUserDetails(null);
+                    }
+                },
+                (error) => {
+                    console.error("Error fetching user data:", error);
+                    setUserDetails(null);
+                }
+            );
+
+            const animalUnsubscribe = onSnapshot(
+                doc(db, "animals", animalId),
+                async (animalDoc) => {
+                    if (animalDoc.exists()) {
+                        const animalData = animalDoc.data();
+                        const animalImage = await fetchAndCacheImage(
+                            animalData.imageUrls && animalData.imageUrls.length > 0
+                                ? animalData.imageUrls[0]
+                                : null,
+                            "animal"
+                        );
+                        setAnimalDetails({ ...animalData, image: animalImage });
+                    } else {
+                        console.warn("Animal document does not exist");
+                        setAnimalDetails(null);
+                    }
+                },
+                (error) => {
+                    console.error("Error fetching animal data:", error);
+                    setAnimalDetails(null);
+                }
+            );
+
+            //will stop the loading state
+            setLoading(false);
+
+            //unsubscribing from the user and animal data
+            return () => {
+                userUnsubscribe();
+                animalUnsubscribe();
+                console.log("Unsubscribed from user and animal data");
+            };
+        };
+
+        //fetching the data
+        fetchData();
+    }, [userId, animalId]);
+
+    const fetchAndCacheImage = async (imageUrl, type) => {
+        if (!imageUrl) return null;
+
+        const storage = getStorage();
+        const cacheDir = `${FileSystem.cacheDirectory}${type}_images/`;
+        const fileName = imageUrl.split("/").pop().replace(/%2F/g, "_");
+        const cacheFilePath = `${cacheDir}${fileName}`;
+
+        try {
+            await FileSystem.makeDirectoryAsync(cacheDir, { intermediates: true });
+            const fileInfo = await FileSystem.getInfoAsync(cacheFilePath);
+
+            //if the cached file exists, will return the cached file uri
+            if (fileInfo.exists) {
+                return fileInfo.uri;
+            }
+
+            //referencing the image url in the storage
+            const storageRef = ref(storage, imageUrl);
+            //getting the download url of the image
+            const downloadUrl = await getDownloadURL(storageRef);
+            //downloading the image and caching it
+            const downloadResult = await FileSystem.downloadAsync(
+                downloadUrl,
+                cacheFilePath
+            );
+            return downloadResult.uri;
+        } catch (error) {
+            console.warn(`Error caching ${type} image:`, error.message);
+            return null;
+        }
     };
 
-    //takes the user id and returns the user details
-    const userDetails = userDetailsDatabase[userId] || {};
+    /**
+     * if the loading state, user details, or animal details are not loaded, will return a loading indicator
+     */
+    if (loading || !userDetails || !animalDetails) {
+        return (
+            <SafeAreaWrapper>
+                <View style={styles.loadingContainer}>
+                    <Text style={styles.loadingText}>Loading...</Text>
+                    <ActivityIndicator size="large" color="#d9cb94" />
+                </View>
+            </SafeAreaWrapper>
+        );
+    }
 
-    //Function to that takes label and value and returns the user details
+    /**
+     * Function that takes a label and value and returns the user details
+     * @param {*} label
+     * @param {*} value
+     * @returns
+     */
     const showUserDetails = (label, value) => (
         <View style={styles.detailItem}>
             <Text style={styles.detailLabel}>{label}:</Text>
@@ -76,23 +167,14 @@ const UserDetailForm = ({ route, navigation }) => {
         </View>
     );
 
-    //states for the notification modal
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    //states for the selection modal
-    const [isSelectionModalVisible, setIsSelectionModalVisible] = useState(false);
-    //state for the selected status
-    const [selectedStatus, setSelectedStatus] = useState(null);
-
     //function that will open the selection modal when the send adoption status button is pressed
     const openSelectionModal = () => {
         //setting the selection modal to true
         setIsSelectionModalVisible(true);
     };
 
-    //function that will send the notification when the status is selected
+    //will send the notification when the status is selected
     const sendNotification = (status) => {
-        //dummy test for now
-
         //the selection modal will disappear
         setIsSelectionModalVisible(false);
         setSelectedStatus(status); //sets the selected status to the status that was selected
@@ -104,17 +186,21 @@ const UserDetailForm = ({ route, navigation }) => {
 
             //will get the random subject
             //eg. "Adoption Status Update: {petName}"
-            const subject = getRandomEmailSubject(status.id, petName);
+            const subject = getRandomEmailSubject(status.id, animalDetails.name);
             //Getting the random body
             //hi so and so, your adoption status for {petName} is {status}
-            const body = getRandomEmailBody(status.id, userName, petName);
+            const body = getRandomEmailBody(
+                status.id,
+                userDetails.fullName,
+                animalDetails.name
+            );
 
             // Compose the email
             const to = userDetails.email;
             const encodedSubject = encodeURIComponent(subject);
             const encodedBody = encodeURIComponent(body);
 
-            //creating the
+            //creating the email url
             const openMailAppUrl = `mailto:${to}?subject=${encodedSubject}&body=${encodedBody}`;
 
             //Open the devices default email client
@@ -131,6 +217,7 @@ const UserDetailForm = ({ route, navigation }) => {
             style={styles.statusItem}
             onPress={() => sendNotification(item)}
         >
+            {/* showing the adoption status */}
             <Text style={styles.statusText}>{item.label}</Text>
         </TouchableOpacity>
     );
@@ -146,8 +233,11 @@ const UserDetailForm = ({ route, navigation }) => {
                     </TouchableOpacity>
                     {/* content of the header showing the pet name and image */}
                     <View style={styles.headerContent}>
-                        <Image source={{ uri: petImage }} style={styles.headerPetImage} />
-                        <Text style={styles.headerPetName}>{petName}</Text>
+                        <Image
+                            source={{ uri: animalDetails.image }}
+                            style={styles.headerPetImage}
+                        />
+                        <Text style={styles.headerPetName}>{animalDetails.name}</Text>
                     </View>
                     <View style={{ width: 24 }} />
                 </View>
@@ -161,23 +251,17 @@ const UserDetailForm = ({ route, navigation }) => {
                         showsVerticalScrollIndicator={false}
                     >
                         <View style={styles.userInfoContainer}>
-                            <Image source={{ uri: userImage }} style={styles.userImage} />
-                            <Text style={styles.userName}>{userName}</Text>
+                            <Image
+                                source={{ uri: userDetails.image }}
+                                style={styles.userImage}
+                            />
+                            <Text style={styles.userName}>{userDetails.fullName}</Text>
                         </View>
                         {/* form container showing the user details */}
                         <View style={styles.formContainer}>
                             {showUserDetails("Email", userDetails.email)}
-                            {showUserDetails("Phone", userDetails.phone)}
-                            {showUserDetails("Address", userDetails.address)}
-                            {showUserDetails("Occupation", userDetails.occupation)}
-                            {showUserDetails(
-                                "Reason for Adoption",
-                                userDetails.reasonForAdoption
-                            )}
-                            {showUserDetails(
-                                "Previous Pet Experience",
-                                userDetails.previousPetExperience
-                            )}
+                            {showUserDetails("Phone", userDetails.phoneNo)}
+                            {showUserDetails("Address", userDetails.location)}
                         </View>
 
                         {/* when the notifcation button is pressed the sendNotification function is called */}
@@ -194,7 +278,7 @@ const UserDetailForm = ({ route, navigation }) => {
 
                 {/* Selection Modal */}
                 <Modal
-                    animationType="slide"//slides up from the bottom
+                    animationType="slide" //slides up from the bottom
                     transparent={true}
                     visible={isSelectionModalVisible}
                     onRequestClose={() => setIsSelectionModalVisible(false)}
@@ -205,9 +289,9 @@ const UserDetailForm = ({ route, navigation }) => {
                             <Text style={styles.modalTitle}>Select Adoption Status</Text>
                             {/* Adoption statuses list */}
                             <FlatList
-                                data={getAdoptionStatuses()}//calling the function to get the adoption statuses
-                                renderItem={showStatusItem}//function to show the adoption statuses
-                                keyExtractor={(item) => item.id}//extracting the id of the status which is found in the getAdoptionStatuses function
+                                data={getAdoptionStatuses()} //calling the function to get the adoption statuses
+                                renderItem={showStatusItem} //function to show the adoption statuses
+                                keyExtractor={(item) => item.id} //extracting the id of the status which is found in the getAdoptionStatuses function
                             />
                             <TouchableOpacity
                                 style={styles.closeButton}
