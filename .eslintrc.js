cf3b419c095e9ee94023c9bfe479a30b4941a619
Caleb Baard <wordwarriors52@yq0fx.onmicrosoft.com>
2024-11-20 16:12:01 +0200

    test ci/cd pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,33 @@
- 
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    "react-native/react-native": true,
+  },
+  plugins: ["react", "react-native"],
+  rules: {
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "no-unused-vars": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
